6c0be924669f8f8ce04626b0a3dc6b27af2b1a78
sasasak <bokyong97@naver.com>
2026-08-27 10:23:41 +0900

    문제풀이: [프로그래머스] lv.0 문자열 돌리기

---
+++ level_0/day_02/03_문자열_돌리기/solution.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close', function () {
+    const str = input[0]; 
+
+    str.split('').forEach(char => {
+        console.log(char);
+    });
+});
