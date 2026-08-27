9b569673b7f140aad2c53cc3e83fcfefd0d2f925
sasasak <bokyong97@naver.com>
2026-08-27 09:43:33 +0900

    문제풀이: [프로그래머스] lv.0 문자열 붙여서 출력하기

---
+++ level_0/day_02/02_문자열_붙여서_출력하기/solution2.js
@@ -0,0 +1,14 @@
+// 다른사람 풀이 참고 
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line.split(' ');
+}).on('close', function () {
+  console.log(input.join(''));
+});
